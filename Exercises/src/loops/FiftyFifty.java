81a6cd63f48144b9936ec9c6ed6d1cdcf06e1f6e
= <mbenba01@mail.bbk.ac.uk>
2014-10-13 19:04:01 +0100

    Third commit

--- Exercises/src/loops/FiftyFifty.java
+++ Exercises/src/loops/FiftyFifty.java
@@ -11,10 +11,10 @@ public class FiftyFifty {
 		// TODO Auto-generated method stub
 		int sum = 0;
 		int i = 0;
-		while(i <= 100) {
+		do {
 			sum += i;
 			i++;
-		}
+		} while(i <= 100); 
 		System.out.print(sum);
 	}
 
