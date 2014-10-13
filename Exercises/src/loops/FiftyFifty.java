49d6ee706904e4bb77b7da2dbdacebe516cc039a
Mustapha Benbaziz <mbenba01@mail.bbk.ac.uk>
2014-10-13 18:35:53 +0100

    First commit from eclipse

---
+++ Exercises/src/loops/FiftyFifty.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class FiftyFifty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i = 0; i <= 100; i++) {
+			sum += i;
+			
+		}
+		System.out.print(sum);
+	}
+
+}
