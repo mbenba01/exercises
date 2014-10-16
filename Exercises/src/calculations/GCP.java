72ecbb2ddb766532eeac67b0dfa6dd0640644249
= <mbenba01@mail.bbk.ac.uk>
2014-10-16 22:14:02 +0100

    Version 2.0

--- Exercises/src/calculations/GCP.java
+++ Exercises/src/calculations/GCP.java
@@ -5,21 +5,27 @@ public class GCP {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 72;
-		int y = 54;
+		int x = 48;
+		int y = 72;
 		int larger = x;
 		int smaller = y;
-		int diff = larger - smaller;
-		while(diff != 0) {
-			if(smaller > larger) {
-				larger = smaller;
-			}
-			diff = larger - smaller;
-			larger = diff;
+		if( y > x) {
+			larger = y;
+			smaller = x;
 		}
-		System.out.print(diff);
+		int result = 0;
+		for(int i = 0; i < larger; i++) {
+				
+				result = larger - smaller;	
+				larger = smaller;
+				smaller = result;
+				
+				if(result <= 0) 
+					System.out.println("result: " + Math.abs(smaller));	
+							
 		
-		
+		}
+		//System.out.println("result: " + larger);
 	}
 	
 }
