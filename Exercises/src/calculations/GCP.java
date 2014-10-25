30ec5e868b86b0694d44cd2f0fc2be7dcf4bdd4c
Mustapha Benbaziz <mbenba01@mail.bbk.ac.uk>
2014-10-25 16:35:15 +0100

    Solution to GCD problem using modulus

--- Exercises/src/calculations/GCP.java
+++ Exercises/src/calculations/GCP.java
@@ -1,31 +1,30 @@
 package calculations;
-
+import java.util.Scanner;
 public class GCP {
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x = 48;
-		int y = 72;
-		int larger = x;
-		int smaller = y;
-		if( y > x) {
-			larger = y;
-			smaller = x;
-		}
-		int result = 0;
-		for(int i = 0; i < larger; i++) {
-				
-				result = larger - smaller;	
-				larger = smaller;
-				smaller = result;
-				
-				if(result <= 0) 
-					System.out.println("result: " + Math.abs(smaller));	
-							
 		
-		}
-		//System.out.println("result: " + larger);
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter first number:");
+		int number1 = in.nextInt();
+		System.out.println("Please enter Second number:");
+		int number2 = in.nextInt();
+		in.close();
+		
+		System.out.println("GCD of two numbers " + number1 + " and " + number2 + " is " + findGCD(number1, number2));
 	}
+
+	private static int findGCD(int number1, int number2) {
+
+		if(number2 == 0) {
+			return number1;
+		}
+		
+		return findGCD(number2, number1%number2);
+	}
+		
 	
+	
+
 }
